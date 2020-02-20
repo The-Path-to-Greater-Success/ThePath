7f6685913f0237f4c651e08f5f3840ffd7a9d15d
unknown <mystiskmaskerade@gmail.com>
2020-02-20 23:57:46 +0100

    Initial Unity Setup

---
+++ Assets/SimpleMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    // Public Variables
+    public float Velocity = 1.0f;
+    public float JumpForce = 1.0f;
+    // Private Variables
+    private Rigidbody _rigidbody;
+    private float _horizontalSpeed;
+    private float _verticalSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontalSpeed = Input.GetAxis("Horizontal") * Velocity;
+        _verticalSpeed = Input.GetAxis("Vertical") * Velocity;
+        _rigidbody.AddForce(_horizontalSpeed, 0, _verticalSpeed);
+    }
+
+}
